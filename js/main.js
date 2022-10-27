49956b8804f108a404599175755e300c9c9a8e41
LorenzoMoccia <moccialorenzo98@gmail.com>
2022-10-27 15:38:56 +0200

    Terminato

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const listaSpesa = ["Vino", "Birra", "Vodka", "Scampi", "Gin"];
+
+const shoppingList = document.getElementById("lista-spesa");
+
+let i = 0;
+
+while(i <= listaSpesa.length - 1){
+    let item = document.createElement("li");
+
+    item.innerHTML = listaSpesa[i];
+
+    shoppingList.append(item);
+
+    i++;
+}
